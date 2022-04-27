0d27f74b13e8f432d573fdeb1a2cab8c43a832ac
Davide Ragazzon <davide_ragazzon@mckinsey.com>
2022-04-27 14:43:10 +0200

    Take care of points outside boundaries in the data cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -20,6 +20,9 @@ def perform_simple_cleaning(df, min_price=10, max_price=350):
     df = df[idx].copy()
     # Convert last_review to datetime
     df["last_review"] = pd.to_datetime(df["last_review"])
+    # Keep only points inside boundaries
+    idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].between(40.5, 41.2)
+    df = df[idx].copy()
     return df
 
 
